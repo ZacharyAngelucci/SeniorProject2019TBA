499a2d97f5bd69ed882c474f4839ad2b0c735c0f
eggnog26 <32025750+eggnog26@users.noreply.github.com>
2019-02-18 17:37:50 -0500

    Update address.py
    updated randstreet() and randzip() to return random set of charecters

--- datagen/address.py
+++ datagen/address.py
@@ -1,10 +1,12 @@
 import random
+import string
 
-def randaddress():
+def randaddress(size=6, chars=string.ascii_uppercase + string.digits):
     """Returns a random address in the following format
     [Street, city/town, 2 letter state, Zip code]
     """
-    #TODO 
+
+    #TODO
     # Might be hard to generate each component individually is there a way to
     # pull a random address from somewhere?
     # https://openaddresses.io/
@@ -13,24 +15,21 @@ def randaddress():
 
 def randstate():
     """Returns a random state in abbreviated form"""
-    states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
-                "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
-                "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
-                "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+    states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+                "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+                "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+                "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
                 "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
     return random.choice(states)
 
-def randstreet():
-    """Returns a random street name and number"""
-    street = str(random.randrange(0,9999))
-    #TODO 
-    # How should we generate street names?
+def randstreet(size=8, chars=string.ascii_lowercase):
+    """Returns a random street name and number ("hviwen")"""
+    return ''.join(random.choice(chars) for _ in range(size))
 
-    return street
-
-def randzip():
-    """Returns a random zipcode"""
+def randzip(size=5, string.digits):
+    """Returns a random zipcode ("85734")"""
+    return ''.join(random.choice(chars) for _ in range(size))
     #TODO
-    # Over 42,000 zip codes in the US, I don't see an easy way of pulling from a 
-    # list of valid codes.  Might be easier to generate 
-    pass
+    # Over 42,000 zip codes in the US, I don't see an easy way of pulling from a
+    # list of valid codes.  Might be easier to generate
+pass
