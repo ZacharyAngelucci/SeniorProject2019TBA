89c828a92ec374f03e731afaf0af755e4bead8fe
Tobie <tobias.rittgers@my.ccsu.edu>
2019-02-27 20:12:41 -0500

    Fixed address generation
    Was using old code and not yagnas updated random address method,

--- subscripts/aboutyou.py
+++ subscripts/aboutyou.py
@@ -4,21 +4,21 @@ import random
 
 from datagen import addressgen, persongen
 
-HEADER = ["FirstName","MiddleName","LastName","Address","Apartment",
-			"DateOfBirth", "Email", "Phone Number"]
+HEADER = ["FirstName", "MiddleName", "LastName", "Address", "Apartment",
+          "DateOfBirth", "Email", "Phone Number"]
+
 
 def makeList():
-	"""	Returns the randomly generated fields in the "About You" page, according
-		to the DSA template
-	"""
-	address = addressgen.randaddress() # ['street', zipcode', 'City', 'state']
-	state = address[3]
-	firstname = state + "CCSUFN"
-	lastname = state + "CCSULN"
-	#Does everyone need an apartment number?
-	apartment = random.randint(0, 9999)
-	email = "plautomation@thehartford.com"
-	return [state, firstname, persongen.randMI(), lastname, ", ".join(map(str, address)),
-			apartment, persongen.randDOB(), email, persongen.randphonenum()]
-
-#print(makeList())
+    """	Returns the randomly generated fields in the "About You" page, according
+            to the DSA template
+    """
+    address = addressgen.randaddress(
+    )  # [Street, city/town, 2 letter state, Zip code]
+    state = address[3]
+    firstname = state + "CCSUFN"
+    lastname = state + "CCSULN"
+    # Does everyone need an apartment number?
+    apartment = random.randint(0, 9999)
+    email = "plautomation@thehartford.com"
+    return [state, firstname, persongen.randMI(), lastname, ", ".join(map(str, address)),
+            apartment, persongen.randDOB(), email, persongen.randphonenum()]
