83c1526f7641fdd5327607ef2a589f49ab39995b
eggnog26 <32025750+eggnog26@users.noreply.github.com>
2019-04-03 20:21:47 -0400

    Updated DSAWebTest.py
    Added the ability to feed csv file
    Also cleaned up the code
    stills stuck on drop down box on "your drivers" page

--- Selenium/DSAWebTest.py
+++ Selenium/DSAWebTest.py
@@ -1,49 +1,54 @@
 import time
+import csv
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common import actions
 from selenium.webdriver.common.by import By
 
-# create a new Firefox session
-driver = webdriver.Chrome()
+# Read from CSV file
+# Currently using sample test data from testData.csv
+with open('testData.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        firstName = row[0]
+        lastName = row[1]
+        dob = row[2]
+        address = row[3]
+
+# create a new Firefox/Chrome session
+driver = webdriver.Firefox()
+# driver = webdriver.Chrome()
 driver.implicitly_wait(30)
-driver.maximize_window()
 
 # Navigate to the application home page
-driver.get("DSA APP GOES HERE")
+driver.get("DSA APP Goes Here")
 
-#Testing DSA Template Data
+# Testing DSA Template Data
 
-# AboutYou Page
+# ABOUT YOU PAGE
 # First Name
-search_field = driver.find_element_by_id("firstName")
-search_field.clear()
-search_field.send_keys("Bob")
+driver.find_element_by_id("firstName").send_keys(firstName)
 
 # Last Name
-search_field = driver.find_element_by_id("lastName")
-search_field.clear()
-search_field.send_keys("McAdoo")
+driver.find_element_by_id("lastName").send_keys(lastName)
 
 # DOB
-search_field = driver.find_element_by_id("dateOfBirth")
-search_field.clear()
-search_field.send_keys("04/26/1997")
+driver.find_element_by_id("dateOfBirth").send_keys(dob)
 
 # Street Address
-search_field = driver.find_element_by_id("aboutMeAddress")
-search_field.clear()
-search_field.send_keys("226 Main St")
+driver.find_element_by_id("aboutMeAddress").send_keys(address)
 
 # About You - Click Next
-nextbutton = driver.find_element_by_id("about-you-next-button")
-nextbutton.click()
+driver.find_element_by_id("about-you-next-button").click()
 time.sleep(1)
+
 # popup confirmation
-# Once finished with Aboutyou page, a pop up window is appearing with saved profiles.
-# This dismisses that
-button = driver.find_element_by_id("modal_btn_icon")
-button.click()
+# This dismisses a pop up window appears with saved profiles.
+driver.find_element_by_id("modal_btn_icon").click()
 time.sleep(5)
 
+
+# DRIVER PAGE
 # Add a Driver Button
 buttonclick = driver.find_element_by_class_name('add-vehicle-label')
 action = webdriver.common.action_chains.ActionChains(driver)
@@ -53,44 +58,37 @@ action.perform()
 time.sleep(1)
 
 # Select Yes for Vehicle Identification Number
-
-search_field = driver.find_element_by_id("vinQuestion_div_add_0")
-search_field.click()
-
+driver.find_element_by_id("vinQuestion_div_add_0").click()
 
 # Enter VIN Number
-search_field = driver.find_element_by_id("vehicleVin")
-search_field.clear()
-search_field.send_keys("JTHBD182610032265")
-time.sleep(1)
+driver.find_element_by_id("vehicleVin").send_keys("JTHBD182610032265")
+time.sleep(1)  # Don't remove
 
 # Press Add Vehicle
-nextbutton = driver.find_element_by_class_name("primary-cta")
-nextbutton.click()
-time.sleep(1)
+driver.find_element_by_class_name("primary-cta").click()
 
 # Confirm added driver
-nextbutton = driver.find_element_by_id("vehicles-next-button")
-nextbutton.click()
+driver.find_element_by_id("vehicles-next-button").click()
 
 # Second Confirmation Pop up
-search_field = driver.find_element_by_class_name("primary-cta")
-search_field.click()
-time.sleep(1)
+search_field = driver.find_element_by_class_name("primary-cta").click()
 
 # Select Paid for
-search_field = driver.find_element_by_id("vehicleOwnership_1_div_1_0")
-search_field.click()
+driver.find_element_by_id("vehicleOwnership_1_div_1_0").click()
 
 # Select yes for registration
 driver.find_element_by_xpath("//*[@id='vehicleRegistered_1_div_1_0']").click()
 
 # STOPS WORKING HERE
+#buttonclick = driver.find_element_by_class_name('col-xs-12')
+#action = webdriver.common.action_chains.ActionChains(driver)
+#action.move_to_element_with_offset(buttonclick, 10, 10)
+#action.click()
+#action.perform()
+#selecto = driver.find_element_by_xpath("//ul[@id='dropdownId']//li[. = 'Commute to work/school']")
+#selecto.click()
 
-# Select vehicle use drop down menu
-buttonclick = driver.find_element_by_id('vehicleRegistered_1_div_1_1')
-action = webdriver.common.action_chains.ActionChains(driver)
-action.move_to_element_with_offset(buttonclick, 5, 5)
-action.click()
-action.perform()
+image = driver.find_element_by_id("col-xs-12 dropup")
+driver.execute_script("arguments[0].value = 'col-xs-12 dropup open';", image)
 
+#driver.close()
