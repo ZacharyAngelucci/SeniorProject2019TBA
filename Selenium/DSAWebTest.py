a7f45788d28e9bbe21e7f1170ce83931cf42fc05
eggnog26 <32025750+eggnog26@users.noreply.github.com>
2019-04-05 17:34:25 -0400

    DSAWebTest.py Updated
    Selenium Web Test has been updated to fully remove all hard coded data and rely completly on the input CSV file. Fixed issues with unexpected pop ups.
    These options need to be removed from data gen
    Options that create more options
    -Commute to work/school
    -"yes" for Defensive Driving

--- Selenium/DSAWebTest.py
+++ Selenium/DSAWebTest.py
@@ -4,28 +4,49 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common import actions
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 # Read from CSV file
 # Currently using sample test data from testData.csv
 from selenium.webdriver.support.wait import WebDriverWait
-
 with open('testData.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
-        firstName = row[0]
-        lastName = row[1]
-        dob = row[2]
-        address = row[3]
+        firstName = row[2]
+        lastName = row[4]
+        dob = row[7]
+        address = row[5]
+        vin = row[12]
+        vOwnership = row[13]
+        vResgistered = row[14]
+        vUsage = row[15]
+        gender = row[18]
+        marital = row[20]
+        employment = row[19]
+        residence = row[21]
+        lastFive = row[22]
+        accident = row[23]
+        defence = row[24]
+        smallBusiness = row[25]
+        currInsured = row[26]
+        bodyCoverage = row[27]
+        polExpiry = row[28]
+        polterm = row[29]
 
+# splits address into individual fields
+address = address.split(',')
+streetAd = address[0]
+zipCode = address[3][1:]
+print(zipCode)
 # create a new Firefox/Chrome session
 driver = webdriver.Firefox()
-# driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
 driver.implicitly_wait(30)
-
 # Navigate to the application home page
-driver.get("DSA APP Goes Here")
 
 # Testing DSA Template Data
+driver.get("https://qa-quote.thehartford.com/sales/landing-page?zip="+zipCode+"&PLCode=002121&organic=true&affinity=AARP&lob=Auto")
+
 
 # ABOUT YOU PAGE
 # DOB
@@ -38,7 +59,7 @@ driver.find_element_by_id("firstName").send_keys(firstName)
 driver.find_element_by_id("lastName").send_keys(lastName)
 
 # Street Address
-driver.find_element_by_id("aboutMeAddress").send_keys(address)
+driver.find_element_by_id("aboutMeAddress").send_keys(streetAd)
 
 # About You - Click Next
 driver.find_element_by_id("about-you-next-button").click()
@@ -46,8 +67,8 @@ time.sleep(2)
 
 # popup confirmation
 # This dismisses a pop up window appears with saved profiles.
+
 driver.find_element_by_id("modal_btn_icon").click()
-time.sleep(5)
 
 
 # DRIVER PAGE
@@ -63,7 +84,7 @@ time.sleep(1)
 driver.find_element_by_id("vinQuestion_div_add_0").click()
 
 # Enter VIN Number
-driver.find_element_by_id("vehicleVin").send_keys("JTHBD182610032265")
+driver.find_element_by_id("vehicleVin").send_keys(vin)
 time.sleep(1)  # Don't remove
 
 # Press Add Vehicle
@@ -76,14 +97,21 @@ driver.find_element_by_id("vehicles-next-button").click()
 search_field = driver.find_element_by_class_name("primary-cta").click()
 
 # Select Paid for
-driver.find_element_by_id("vehicleOwnership_1_div_1_0").click()
+if vOwnership == "Paid For":
+    driver.find_element_by_id("vehicleOwnership_1_div_1_0").click()
+elif vOwnership == "Financed":
+    driver.find_element_by_id("vehicleOwnership_1_div_1_1").click()
+else:
+    driver.find_element_by_id("vehicleOwnership_1_div_1_2").click()
 
 # Select yes for registration
-driver.find_element_by_xpath("//*[@id='vehicleRegistered_1_div_1_0']").click()
+if vResgistered == "yes":
+    driver.find_element_by_xpath("//*[@id='vehicleRegistered_1_div_1_0']").click()
+else:
+    driver.find_element_by_xpath("//*[@id='vehicleRegistered_1_div_1_1']").click()
 
 # Vehicle Usage Drop Down
-
-dropDown = driver.find_element_by_xpath("//a[@id='vehicleUsage_1' and contains(.,'Pleasure')]")
+dropDown = driver.find_element_by_xpath("//a[@id='vehicleUsage_1' and contains(.,'"+vUsage+"')]")
 driver.execute_script("arguments[0].click();",dropDown)
 
 # Press Save Vehicle
@@ -103,25 +131,31 @@ driver.find_element_by_id("allDriversListed_div__0").click()
 
 # Press Continue Button on Confirm your Drivers
 driver.find_element_by_xpath("//*[@class='btn btn-primary']").click()
-time.sleep(2)
+time.sleep(5)
 # Your Drivers
 # Select Male or Female
-driver.find_element_by_id("gender_div_0_0").click()
+if gender == "Male":
+    driver.find_element_by_id("gender_div_0_0").click()
+else:
+    driver.find_element_by_id("gender_div_0_1").click()
 
 # Select Marital Status
-dropDown = driver.find_element_by_xpath("//a[@id='maritalStatus' and contains(.,'Married')]")
+dropDown = driver.find_element_by_xpath("//a[@id='maritalStatus' and contains(.,'"+marital+"')]")
 driver.execute_script("arguments[0].click();",dropDown)
 
 # Select Employment Status
-dropDown = driver.find_element_by_xpath("//a[@id='employmentStatus' and contains(.,'Employed')]")
+dropDown = driver.find_element_by_xpath("//a[@id='employmentStatus' and contains(.,'"+employment+"')]")
 driver.execute_script("arguments[0].click();",dropDown)
 
 # Select Residence
-dropDown = driver.find_element_by_xpath("//a[@id='primaryResidence' and contains(.,'I own my home')]")
+dropDown = driver.find_element_by_xpath("//a[@id='primaryResidence' and contains(.,'"+residence+"')]")
 driver.execute_script("arguments[0].click();",dropDown)
 
 # Yes/No Have you lived there for at least 5 years
-driver.find_element_by_id("livedFiveYears_div_0_0").click()
+if lastFive == "Yes":
+    driver.find_element_by_id("livedFiveYears_div_0_0").click()
+else:
+    driver.find_element_by_id("livedFiveYears_div_0_1").click()
 
 # Have you been licensed for at least 3 years?
 dropDown = driver.find_element_by_xpath("//a[@id='licensedDuration' and contains(.,'Less than 1 year')]")
@@ -130,20 +164,30 @@ time.sleep(5)
 #driver.find_element_by_id("icon icon-close").click()
 
 # During the past 5 years, have you had an accident (regardless of fault), violation or claim?
-driver.find_element_by_id("violation_div_0_1").click()
+if accident == "Yes":
+    driver.find_element_by_id("violation_div_0_0").click()
+else:
+    driver.find_element_by_id("violation_div_0_1").click()
+
 
 # Have you maintained a B (or better) grade point average and/or part of the top 20% of your class?
-driver.find_element_by_id("goodStudent_div_0_1").click()
+# driver.find_element_by_id("goodStudent_div_0_1").click()
 
 # Have you taken a Defensive Driving course in the past 3 years?
-driver.find_element_by_id("defensiveDriving_div_0_1").click()
+if defence == "Yes":
+    driver.find_element_by_id("defensiveDriving_div_0_0").click()
+else:
+    driver.find_element_by_id("defensiveDriving_div_0_1").click()
 
 # Do you or your listed drivers own or work for a small business?
-driver.find_element_by_id("ownSmallBusiness_div_0_1").click()
+if defence == "Yes":
+    driver.find_element_by_id("ownSmallBusiness_div_0_0").click()
+else:
+    driver.find_element_by_id("ownSmallBusiness_div_0_1").click()
 
 # Press Save Driver Button
 driver.find_element_by_id("saveButton_0").click()
-time.sleep(5)
+time.sleep(10)# wait for loading screen to go away
 
 # Are you an AARP member
 driver.find_element_by_id("aarpMembership_div_aarp_1").click()
@@ -152,24 +196,24 @@ driver.find_element_by_id("aarpMembership_div_aarp_1").click()
 driver.find_element_by_id("drivers-next-button").click()
 
 # Not adding your spouse/domestic partner
-dropDown = driver.find_element_by_xpath("//a[@id='spouse' and contains(.,'Permanently living outside the household for more than 1 year')]")
-driver.execute_script("arguments[0].click();",dropDown)
-
-# Click Continue on spouse pop up
-driver.find_element_by_xpath("//*[@class='btn btn-primary']").click()
+if marital == "Married":
+    print(marital)
+    dropDown = driver.find_element_by_xpath("//a[@id='spouse' and contains(.,'Permanently living outside the household for more than 1 year')]")
+    driver.execute_script("arguments[0].click();",dropDown)
+    driver.find_element_by_xpath("//*[@class='btn btn-primary']").click()
 
 # Are you currently insured?
-dropDown = driver.find_element_by_xpath("//a[@id='currentInsurance' and contains(.,'Yes, with your own policy')]")
+dropDown = driver.find_element_by_xpath("//a[@id='currentInsurance' and contains(.,'"+currInsured+"')]")
 driver.execute_script("arguments[0].click();",dropDown)
 time.sleep(2)
 
 # What is your current range of bodily injury coverage? If you're unsure, take your best guess
-dropDown = driver.find_element_by_xpath("//a[@id='currentBodilyInjuryRange' and contains(.,'$20,000 / $40,000')]")
+dropDown = driver.find_element_by_xpath("//a[@id='currentBodilyInjuryRange' and contains(.,'"+bodyCoverage+"')]")
 driver.execute_script("arguments[0].click();",dropDown)
 time.sleep(2)
 
 # When will your current policy expire?
-driver.find_element_by_id("dateField").send_keys("06/06/2019")
+driver.find_element_by_id("dateField").send_keys(polExpiry)
 
 # How long have you been with your current carrier?
 dropDown = driver.find_element_by_xpath("//a[@id='yearsCurrentInsurance' and contains(.,'1 year')]")
@@ -177,5 +221,8 @@ driver.execute_script("arguments[0].click();",dropDown)
 time.sleep(2)
 
 # Is your policy 6 or 12 months long?
-driver.find_element_by_id("currentTermDurationPeriod_div__0").click()
+if polterm == "6 months":
+    driver.find_element_by_id("currentTermDurationPeriod_div__0").click()
+else:
+    driver.find_element_by_id("currentTermDurationPeriod_div__1").click()
 #driver.close()
